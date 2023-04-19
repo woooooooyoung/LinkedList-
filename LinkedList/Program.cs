bf2542ab7c79cf2f86f51f2aae550709aa16eacd
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-20 05:12:08 +0900

    111

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace LinkedList
-{
+{   
+    // 1. LinkedList 구현해보기
+    // AddFirst, AddLast,
+    // AddBefore, AddAfter,
+    // Remove(T value), Remove(node), Find
+    // + 주석 추가
+    // -------------------------------
+    // 2. LinkedList 기술면접 준비
+    // -------------------------------
+    // 0. msdn C# LinkedList 참고해서
+    // Contains, FindLast, RemoveFirst, RemoveLast
+
     public class LinkedListNode<T>
     {
         public LinkedList<T> list;
         public LinkedListNode<T> prev;
         public LinkedListNode<T> next;
         public T item;
-        public int mp;
 
         public LinkedListNode(T value)
         {
@@ -37,7 +48,7 @@ namespace LinkedList
         public LinkedList<T> List { get { return list; } }
         public LinkedListNode<T> Prev { get { return prev; } }
         public LinkedListNode<T> Next { get { return next; } }
-        public T Value { get { return item; } set { item = value; } }
+        public T Item { get { return item; } set { item = value; } }
     }
 
     public class LinkedList<T>
@@ -57,43 +68,101 @@ namespace LinkedList
         private LinkedListNode<T> Last { get { return tail; } }
         public int Count { get { return count; } }
 
-        public LinkedListNode<T> AddFirst(T value)
-        {
-            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
-
-            if (head != null)
-            {
-                newNode.next = head;
-                head.prev = newNode;
-                head = newNode;
-            }
-            else
-            {
-                head = newNode;
-                tail = newNode;
-            }
-            count++;
-            return newNode;
-        }
+        // AddBefore
         public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
         {
+            ValidateNode(node);
             LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+            InsertNodeBefore(node, newNode);
             if (node == head)
                 head = newNode;
             return newNode;
         }
 
-        public LinkedListNode<T> AddLast(LinkedListNode<T> node, T value)
-        {
-            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
-
-            return newNode;
-        }
+        // AddAfter
         public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)
         {
+            ValidateNode(node);
             LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+            InsertNodeAfter(node, newNode);
+            if (node == tail)
+                tail = newNode;
             return newNode;
         }
+
+        // AddFirst
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+            if (head != null)
+            {
+                InsertNodeBefore(head, newNode);
+                head = newNode;
+            }
+            else
+            {
+                InsertNodeToEmptyList(newNode);
+            }
+            return newNode;
+        }
+
+        // AddLast
+        public LinkedListNode<T> AddLast(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+            if (tail != null)
+            {
+                InsertNodeAfter(tail, newNode);
+                tail = newNode;
+            }
+            else
+            {
+                InsertNodeToEmptyList(newNode);
+            }
+            return newNode;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            return Find(value) != null;
+        }
+
+        // Find
+        public LinkedListNode<T> Find(T value)
+        {
+            LinkedListNode<T>? node = head;
+            EqualityComparer<T> c = EqualityComparer<T>.Default;
+            if (value != null)
+            {
+                while (node != null)
+                {
+                    if (c.Equals(node.Item, value))
+                        return node;
+                    else
+                        node = node.next;
+                }
+            }
+            else
+            {
+                while (node != null)
+                {
+                    if (node.Item == null)
+                        return node;
+                    else
+                        node = node.next;
+                }
+            }
+            return null;
+        }
+
+        // Remove(T value)
         public bool Remove(T value)
         {
             LinkedListNode<T>? node = Find(value);
@@ -108,7 +177,8 @@ namespace LinkedList
             }
         }
 
-        public void RemoveNode(LinkedListNode<T> node)
+        // Remove(node)
+        public void Remove(LinkedListNode<T> node)
         {
             ValidateNode(node);
             if (head == node)
@@ -119,20 +189,78 @@ namespace LinkedList
             // tail과 node가 같을 때 head의 자리에 이전의 노드를 넣어준다.
             RemoveNode(node);
         }
-        public LinkedListNode<T> Find(T value)
-        {
 
+        public void RemoveFirst()
+        {
+            if (head == null)
+                throw new InvalidOperationException();
+
+            LinkedListNode<T> headNode = head;
+            Remove(headNode);
         }
-        public void ValidateNode(LinkedListNode<T> node)
+
+        public void RemoveLast()
+        {
+            if (tail == null)
+                throw new InvalidOperationException();
+
+            LinkedListNode<T> tailNode = tail;
+            Remove(tailNode);
+        }
+
+        private void ValidateNode(LinkedListNode<T> node)
         {
             if (node == null)
                 throw new ArgumentNullException();
             if (node.list != this)
                 throw new InvalidOperationException();
         }
-    
 
+        private void InsertNodeBefore(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            newNode.next = node;
+            newNode.prev = node.prev;
+            if (newNode.prev != null)
+                newNode.prev.next = newNode;
 
+            node.prev = newNode;
+
+            count++;
+        }
+
+        private void InsertNodeAfter(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            newNode.prev = node;
+            newNode.next = node.next;
+            if (newNode.next != null)
+                newNode.next.prev = newNode;
+
+            node.next = newNode;
+
+            count++;
+        }
+
+        private void InsertNodeToEmptyList(LinkedListNode<T> newNode)
+        {
+            if (count != 0)
+                throw new InvalidOperationException();
+
+            head = newNode;
+            tail = newNode;
+            count++;
+        }
+
+        private void RemoveNode(LinkedListNode<T> node)
+        {
+            if (node.list != this)
+                throw new InvalidOperationException();
+
+            if (node.prev != null)
+                node.prev.next = node.next;
+            if (node.next != null)
+                node.next.prev = node.prev;
+
+            count--;
+        }
     }
 }
-
