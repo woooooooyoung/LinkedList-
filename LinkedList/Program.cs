018d4d8382fb77c902e3dfffa3ca1bac46d59aa4
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-19 21:48:04 +0900

    feat : LinkedList
    LinkedList 과제1 미완성

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,10 +1,138 @@
-﻿namespace LinkedList
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LinkedList
 {
-    internal class Program
+    public class LinkedListNode<T>
     {
-        static void Main(string[] args)
+        public LinkedList<T> list;
+        public LinkedListNode<T> prev;
+        public LinkedListNode<T> next;
+        public T item;
+        public int mp;
+
+        public LinkedListNode(T value)
         {
-            Console.WriteLine("Hello, World!");
+            this.list = null;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+            
         }
+        public LinkedListNode(LinkedList<T> list, T value)
+        {
+            this.list = list;
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, LinkedListNode<T> prev, LinkedListNode<T> next, T value)
+        {
+            this.list = list;
+            this.prev = prev;
+            this.next = next;
+            this.item = value;
+        }
+
+        public LinkedList<T> List { get { return list; } }
+        public LinkedListNode<T> Prev { get { return prev; } }
+        public LinkedListNode<T> Next { get { return next; } }
+        public T Value { get { return item; } set { item = value; } }
+    }
+
+    public class LinkedList<T>
+    {
+        public LinkedListNode<T> head;
+        public LinkedListNode<T> tail;
+        public int count;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        private LinkedListNode<T> First { get { return head; } }
+        private LinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            if (head != null)
+            {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            else
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            count++;
+            return newNode;
+        }
+        public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+            if (node == head)
+                head = newNode;
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddLast(LinkedListNode<T> node, T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            return newNode;
+        }
+        public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+            return newNode;
+        }
+        public bool Remove(T value)
+        {
+            LinkedListNode<T>? node = Find(value);
+            if (node != null)
+            {
+                Remove(node);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RemoveNode(LinkedListNode<T> node)
+        {
+            ValidateNode(node);
+            if (head == node)
+                head = node.next;
+            // head와 node가 같을 때 head의 자리에 다음의 노드를 넣어준다.
+            if (tail == node)
+                tail = node.prev;
+            // tail과 node가 같을 때 head의 자리에 이전의 노드를 넣어준다.
+            RemoveNode(node);
+        }
+        public LinkedListNode<T> Find(T value)
+        {
+
+        }
+        public void ValidateNode(LinkedListNode<T> node)
+        {
+            if (node == null)
+                throw new ArgumentNullException();
+            if (node.list != this)
+                throw new InvalidOperationException();
+        }
+    
+
+
     }
 }
+
